b1da34f725522fc4c59c89c775898d5ad3b433c9
Greg Wiley <gwiley@cj.com>
2018-12-01 15:18:48 -0800

    log entrypoint

--- update-lambda-2.sh
+++ update-lambda-2.sh
@@ -1,4 +1,5 @@
+
 aws lambda update-function-configuration \
       --function-name bash-runtime \
-      --layers arn:aws:lambda:us-east-1:845290112482:layer:bash-runtime:2
+      --layers "arn:aws:lambda:us-east-1:845290112482:layer:bash-runtime:${1}"
 
